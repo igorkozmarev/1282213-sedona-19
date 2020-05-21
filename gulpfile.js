3acc2ac6f6bdeeb1f9900a8d65e1e2a0aa9ff2ff
Igor Kozmarev <i.kozmarev1@gmail.com>
2020-05-21 20:21:32 +0200

    Собирает проект

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task("css", function () {
     .pipe(csso())
     .pipe (rename ("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
@@ -37,7 +37,7 @@ gulp.task("sprite", function () {
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("html", function () {
@@ -45,7 +45,7 @@ gulp.task("html", function () {
     .pipe(posthtml([
       include()
     ]))
-    .pipe(gulp.dest("source"));
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task ("images", function (){
@@ -82,7 +82,7 @@ gulp.task("clean", function(){
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -90,8 +90,20 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite","html","refresh"));
+  gulp.watch("source/*.html", gulp.series("html","refresh"));
 });
 
-gulp.task ("build", gulp.series("css","sprite","html"));
-gulp.task("start", gulp.series("build ", "server"));
+gulp.task ("refresh", function(done) {
+  server.reload();
+  done();
+});
+
+gulp.task ("build", gulp.series("clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+));
+
+gulp.task("start", gulp.series("build","server"));
