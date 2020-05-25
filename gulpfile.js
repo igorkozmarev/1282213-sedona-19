d8d9a985c06c8446691a3315c15be183bd1e5c0f
Igor Kozmarev <i.kozmarev1@gmail.com>
2020-05-25 12:29:30 +0200

    Исправляет ошибки

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,6 @@ gulp.task ("copy", function(){
   return gulp.src ([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
     "source/*.ico"
   ], {
     base: "source"
